36e6b289ef4425896aed09cce1f97bd4b3699649
bakseck <seck.bakar@ugb.edu.sn>
2024-02-12 08:18:50 +0000

    fix: test

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
 
 export default function Home() {
     return (
@@ -10,14 +10,14 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Header />
+            {/* <Header /> */}
 
             <main>
                 <h1>Bienvenue sur Mon Blog Islamique</h1>
                 {/* Ici, vous pouvez ajouter vos composants ou contenu */}
             </main>
 
-            <Footer />
+            {/* <Footer /> */}
         </>
     );
 }
